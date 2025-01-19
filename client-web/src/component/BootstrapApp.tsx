53a2cd79ef3bc502b11fd73604aff3967b3f3d5f
Jinwook94 <kimjinwook94@gmail.com>
2025-01-19 19:10:39 +0900

    [client-web] mantine, vanilla extract 관련 코드 삭제 등

--- client-web/src/component/BootstrapApp.tsx
+++ client-web/src/component/BootstrapApp.tsx
@@ -1,21 +1,9 @@
 import { BrowserRouter } from "react-router-dom";
-import { MantineProvider, createTheme } from "@mantine/core";
-import { ProviderBuilder } from "../utils/ProviderBuilder";
+import { ProviderBuilder } from "@/utils/ProviderBuilder";
 import App from "../App";
 
 export default function BootstrapApp() {
-  const mantineTheme = createTheme({
-    fontFamily:
-      "Pretendard, -apple-system, BlinkMacSystemFont, system-ui, sans-serif",
-    primaryColor: "blue",
-  });
-
-  const ProviderWrappedApp = new ProviderBuilder(() => <App />)
-    .wrap(MantineProvider, {
-      theme: mantineTheme,
-      defaultColorScheme: "light" as const,
-    })
-    .build();
+  const ProviderWrappedApp = new ProviderBuilder(() => <App />).build();
 
   return (
     <BrowserRouter>
--- client-web/src/main.tsx
+++ client-web/src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import ReactModal from "react-modal";
-import BootstrapApp from "./component/BootstrapApp.tsx";
-import "@mantine/core/styles.css";
-import "./styles/global.css.ts";
+import "@/index.css"; // Tailwind Global Styles
 
-ReactModal.setAppElement("#root"); // <-- 추가
+import BootstrapApp from "./component/BootstrapApp.tsx";
+
+ReactModal.setAppElement("#root");
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
