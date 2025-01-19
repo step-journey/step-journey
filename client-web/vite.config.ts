d0e79a4bfbac1f8e269b8cdcde8b4a71eb05b5e4
Jinwook94 <kimjinwook94@gmail.com>
2025-01-19 18:28:50 +0900

    [client-web] shadcn 사용 위한 기본 설정
    https://ui.shadcn.com/docs/installation/vite

--- client-web/vite.config.ts
+++ client-web/vite.config.ts
@@ -1,8 +1,21 @@
-import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
-import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import { defineConfig } from "vite";
+import { fileURLToPath } from "node:url";
 
-// https://vite.dev/config/
+/**
+ * ESM 환경에서는 __dirname, __filename 을 기본적으로 제공하지 않음
+ * 따라서 import.meta.url 을 이용해 "현재 파일의 URL"을 얻은 뒤,
+ * fileURLToPath 로 이를 실제 파일 경로로 변환하여 __dirname 을 대체 구현
+ */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin()],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // 절대경로 별칭을 @ → ./src 로 설정
+    },
+  },
 });
