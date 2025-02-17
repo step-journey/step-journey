e2b77fca12939c8434ad4c46fbaed9989d760bd8
Jinwook94 <kimjinwook94@gmail.com>
2025-02-17 15:28:37 +0900

    [server-core] 환경 값 상수 사용

--- server/core/internal/flags/constants.go
+++ server/core/internal/flags/constants.go
@@ -1,12 +1,20 @@
 package flags
 
 const (
-	EnvVarEnvironment = "ENVIRONMENT" // ECS Task Definition "environment" 필드
+	// ECS Task Definition "environment" 필드
+	EnvVarEnvironment = "ENVIRONMENT"
 
-	EnvVarDbUserCredential = "DB_USER_CREDENTIAL" // ECS Task Definition "secrets" 필드 (AWS Secrets Manager 로부터 가져오는 값)
+	// ECS Task Definition "secrets" 필드 (AWS Secrets Manager 로부터 가져오는 값)
+	EnvVarDbUserCredential = "DB_USER_CREDENTIAL"
 	EnvVarDbConnectionInfo = "DB_USER_CONNECTION_INFO"
 	EnvVarOAuthSecret      = "OAUTH_SECRET"
 
-	FlagEnv    = "env" // CLI 플래그 관련 상수 (플래그 이름)
+	// CLI 플래그 이름
+	FlagEnv    = "env"
 	FlagConfig = "config-file"
+
+	// 환경
+	EnvLocal = "local"
+	EnvDev   = "dev"
+	EnvProd  = "prod"
 )
--- server/core/pkg/logger/logger.go
+++ server/core/pkg/logger/logger.go
@@ -6,29 +6,32 @@ import (
 
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"server/internal/flags"
 )
 
 func InitLogger(env string) {
 	zerolog.TimeFieldFormat = time.RFC3339Nano
 
-	// 타임스탬프 함수를 UTC 고정
+	// 타임스탬프 함수를 UTC 기준으로 설정
 	zerolog.TimestampFunc = func() time.Time {
 		return time.Now().UTC()
 	}
 
 	var level zerolog.Level
 	switch env {
-	case "prod":
+	case flags.EnvProd:
 		level = zerolog.InfoLevel
-	case "dev":
+	case flags.EnvDev:
+		level = zerolog.DebugLevel
+	case flags.EnvLocal:
 		level = zerolog.DebugLevel
 	default:
 		level = zerolog.DebugLevel
 	}
 	zerolog.SetGlobalLevel(level)
 
-	// local 환경만 ConsoleWriter 출력, 그 외는 JSON 출력
-	if env == "local" {
+	// 로컬 환경인 경우 ConsoleWriter 사용하고, 그 외는 JSON 출력
+	if env == flags.EnvLocal {
 		log.Logger = log.Output(
 			zerolog.ConsoleWriter{
 				Out:        os.Stderr,
@@ -36,5 +39,6 @@ func InitLogger(env string) {
 			},
 		)
 	}
+
 	log.Info().Msgf("[InitLogger] Logging Level: %s (env=%s)", level.String(), env)
 }
