535d069dc7b16daff0ffe870a0c3331b665b0d84
Jinwook94 <kimjinwook94@gmail.com>
2025-03-04 20:01:12 +0900

    [editor] editor 프로젝트 생성 (React, TypeScript, Vite)

--- client-web/src/pages/Editor/EditorPage.tsx
+++ client-web/src/pages/Editor/EditorPage.tsx
@@ -1,4 +1,3 @@
-// src/pages/Editor/EditorPage.tsx
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
