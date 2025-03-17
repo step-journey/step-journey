f04b99c00c6d9356b24a93acfbb0da185c73dc62
Jinwook94 <kimjinwook94@gmail.com>
2025-03-17 14:20:03 +0900

    [client-web] 불필요한 코드 제거 (chore)

--- client-web/src/features/block/types/blockNoteBaseTypes.ts
+++ client-web/src/features/block/types/blockNoteBaseTypes.ts
@@ -1,12 +1,5 @@
-import { BaseBlock, BaseBlockProperties } from "./baseBlock";
+import { BaseBlock } from "./baseBlock";
 import { DefaultStyleSchema, StyledText } from "@blocknote/core";
-
-// BlockNote 블록들을 위한 공통 프로퍼티
-export interface BlockNoteBlockProperties extends BaseBlockProperties {
-  textColor?: string;
-  backgroundColor?: string;
-  textAlignment?: "left" | "center" | "right" | "justify";
-}
 
 // 모든 BlockNote 기반 블록의 기본 인터페이스
 export type BlockNoteBaseBlock = BaseBlock;
